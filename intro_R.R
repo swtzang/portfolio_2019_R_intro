fb1c457abece26807d0a30ea0d65e02d77399d51
swtzang <swtzang@gmail.com>
2019-03-07 20:46:00 +0800

    0307

--- intro_R.R
+++ intro_R.R
@@ -4,8 +4,10 @@ getSymbols("GOOG", auto.assign = TRUE)
 # There are two primary time series data
 # xts and zoo
 tickers = c("GOOG", "MSFT", "AMZN")
-getSymbols(tickers, auto.assign = TRUE)
-
+getSymbols(tickers, from = "2010-01-01", auto.assign = TRUE)
+data = new.env()
+getSymbols(tickers, from = "2010-01-01", env = data , auto.assign = TRUE)
+#
 str(AMZN)
 class(AMZN)
 tail(AMZN,3)
@@ -13,3 +15,14 @@ names(AMZN)
 ls(AMZN)
 head(AMZN$AMZN.Close)
 AMZN2010_15=AMZN['2010/2015']
+#
+
+
+
+
+
+
+
+
+
+
