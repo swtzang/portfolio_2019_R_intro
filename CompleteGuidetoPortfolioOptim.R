0c7104bd5b4c9114352741808e8b6579cad23b12
swtzang <swtzang@gmail.com>
2024-03-25 12:10:02 +0000

    20240325

--- CompleteGuidetoPortfolioOptim.R
+++ CompleteGuidetoPortfolioOptim.R
@@ -1,6 +1,9 @@
+# Reference: 
 # https://miltonfmr.com/the-complete-guide-to-portfolio-optimization-in-r-part1/
+# https://rforanalytics.com/01-Part_2_portfolio.html
 
-# install.packages(c("cluster","mvoutlier","pastecs","fPortfolio"),repos="http://cran.r-project.org")
+# install.pac
+kages(c("cluster","mvoutlier","pastecs","fPortfolio"),repos="http://cran.r-project.org")
 
 #To update all packages we run:
 # update.packages(repos="http://cran.r-project.org")
